e5b0607dc2beb23317ab09e6a3660b37d81d1659
SimonJay <szyszkakontrast@gmail.com>
2022-12-11 14:35:54 +0100

    Authorization added

--- routes/gyms.js
+++ routes/gyms.js
@@ -3,19 +3,9 @@ import catchAsync from "../utils/catchAsync.js";
 import ExpressError from "../utils/ExpressError.js";
 import Gym from "../models/gym.js";
 import { gymJoiSchema, reviewSchema } from "../joiSchemas.js";
-import isLoggedIn from "../middleware.js";
+import { isLoggedIn, isAuthor, validateGym } from "../middleware.js";
 
 const router = express.Router();
-
-const validateGym = (req, res, next) => {
-  const { error } = gymJoiSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
 
 router.get("/", async (req, res) => {
   console.log(req.isAuthenticated());
@@ -25,10 +15,11 @@ router.get("/", async (req, res) => {
 
 router.post(
   "/new",
-  //isLoggedIn,
+  isLoggedIn,
   validateGym,
   catchAsync(async (req, res, next) => {
     const gym = new Gym(req.body.gym);
+    gym.author = req.user._id;
     await gym.save();
     res.status(200).send(gym._id);
   })
@@ -37,7 +28,9 @@ router.post(
 router.get(
   "/:id",
   catchAsync(async (req, res) => {
-    const gym = await Gym.findById(req.params.id).populate("reviews");
+    const gym = await Gym.findById(req.params.id)
+      .populate({ path: "reviews", populate: { path: "author" } })
+      .populate("author");
     res.send({ gym });
   })
 );
@@ -45,15 +38,19 @@ router.get(
 router.post(
   "/:id/update",
   isLoggedIn,
+  isAuthor,
   validateGym,
   catchAsync(async (req, res, next) => {
-    const gym = await Gym.findByIdAndUpdate(req.params.id, { ...req.body.gym });
-    res.status(200).send(gym._id);
+    const { id } = req.params;
+    const gym = await Gym.findByIdAndUpdate(id, { ...req.body.gym });
+    console.log(gym);
+    return res.status(200).send(id);
   })
 );
 
 router.get(
   "/:id/delete",
+  isAuthor,
   isLoggedIn,
   catchAsync(async (req, res) => {
     const gym = await Gym.deleteOne({ _id: req.params.id });
