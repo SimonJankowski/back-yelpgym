3da5071bade035675439c53b2a01cdfca54e3ca8
SimonJay <szyszkakontrast@gmail.com>
2023-01-14 23:55:51 +0100

    cluster map added

--- models/gym.js
+++ models/gym.js
@@ -1,34 +1,43 @@
 import mongoose from "mongoose";
 import Review from "./review.js";
 const Schema = mongoose.Schema;
+const options = { toJSON: { virtuals: true } };
 
-const GymSchema = new Schema({
-  title: String,
-  images: [{ url: String, filename: String }],
-  price: Number,
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true
+const GymSchema = new Schema(
+  {
+    title: String,
+    images: [{ url: String, filename: String }],
+    price: Number,
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
     },
-    coordinates: {
-      type: [Number],
-      required: true
-    }
-  },
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  },
-  reviews: [
-    {
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review"
-    }
-  ]
+      ref: "User"
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review"
+      }
+    ]
+  },
+  options
+);
+
+GymSchema.virtual("properties.popUpMarkup").get(function () {
+  return `<strong><a href="/gym/${this._id}">${this.title}</a></strong>
+  <p>${this.location}</p>`;
 });
 
 GymSchema.post("findOneAndDelete", async function (doc) {
